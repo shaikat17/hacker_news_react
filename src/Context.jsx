a36007ea0a66ebf3ba2059aa48296cd3a9889d77
shaikat17 <shaikatpal56@gmail.com>
2023-03-27 12:38:41 +0600

    made an small changes

--- src/Context.jsx
+++ src/Context.jsx
@@ -44,7 +44,7 @@ const AppProvider = ({children}) => {
     }, [])
 
     const removeStory = id => {
-        console.log(id)
+        // console.log(id)
         dispatch({ type: REMOVE_STORY, payload: id })
     }
 
